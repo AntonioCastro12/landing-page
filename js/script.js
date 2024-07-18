e1d15830163ddcc151f0946e7bf091d451443185
AntonioCastro1163 <antoniojcc786@gmail.com>
2024-07-18 14:54:36 -0600

    Actualización video

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (form) {
         form.addEventListener('submit', function(event) {
-            event.preventDefault();
+            event.preventDefault(); // Detener el envío del formulario por defecto
 
             // Mostrar la barra de carga
             loadingOverlay.style.display = 'flex';
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     if (formContainer) {
                         formContainer.innerHTML = `
                             <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; background-color: #FCBB04;">
-                                <iframe src="https://www.youtube.com/embed/HwXOBXsPnEE?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="position: absolute; top: 50%; left: 50%; width: 70%; height: 70%; transform: translate(-50%, -50%); background-color: #FCBB04; border: none;"></iframe>
+                                       <iframe src="https://www.youtube.com/embed/HwXOBXsPnEE?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="position: absolute; top: 50%; left: 50%; width: 70%; height: 70%; transform: translate(-50%, -50%); background-color: #FCBB04; border: none;"></iframe>
                             </div>`;
                         
                         formContainer.style.backgroundColor = '#FCBB04';
