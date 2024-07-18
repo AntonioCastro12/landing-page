b80881970e5f669de3b079acee59a3f86b88a879
AntonioCastro1163 <antoniojcc786@gmail.com>
2024-07-18 03:11:43 -0600

    Actualización 3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
-document.getElementById('registroForm').addEventListener('submit', function(event) {
+document.addEventListener('submit', function(event) {
     event.preventDefault();
     
-    var nombre = document.getElementById('nombre').value;
-    var email = document.getElementById('email').value;
-    var telefono = document.getElementById('telefono').value;
-    var ciudad = document.getElementById('ciudad').value;
-
-    var mailto_link = 'mailto:example@example.com?cc=antoniojcc786@gmail.com.com&subject=TEST DFX&body=' +
-                      'Nombre: ' + nombre + '%0A' +
-                      'Email: ' + email + '%0A' +
-                      'Teléfono: ' + telefono + '%0A' +
-                      'Ciudad: ' + ciudad;
     
-    window.location.href = mailto_link;
-
     var formContainer = document.querySelector('.registro');
     formContainer.innerHTML = `
         <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; background-color: #FCBB04;">
