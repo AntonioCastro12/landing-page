182d81e2479bc1bf8df659fbea32f6a960bdfcbe
AntonioCastro1163 <antoniojcc786@gmail.com>
2024-07-18 18:54:09 -0600

    Actualización

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
     if (form) {
         form.addEventListener('submit', function(event) {
             event.preventDefault(); 
-
             
             loadingOverlay.style.display = 'flex';
 
-            
             const formData = new FormData(form);
+            const url = form.action;
 
-            
-            fetch(form.action || '', {
+            console.log('Enviando solicitud a:', url);
+
+            fetch(url, {
                 method: 'POST',
                 body: formData,
                 headers: {
@@ -22,14 +22,13 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .then(response => {
                 if (response.ok) {
-                  
+                    console.log('Respuesta OK');
                     const formContainer = document.querySelector('.registro');
                     if (formContainer) {
                         formContainer.innerHTML = `
                             <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; background-color: #FCBB04;">
                                 <iframe src="https://www.youtube.com/embed/HwXOBXsPnEE?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="position: absolute; top: 50%; left: 50%; width: 70%; height: 70%; transform: translate(-50%, -50%); background-color: #FCBB04; border: none;"></iframe>
                             </div>`;
-                        
                         formContainer.style.backgroundColor = '#FCBB04';
                     } else {
                         console.error('Contenedor del formulario no encontrado.');
@@ -40,7 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .catch(error => console.error('Error en la solicitud:', error))
             .finally(() => {
-                
                 loadingOverlay.style.display = 'none';
             });
         });
