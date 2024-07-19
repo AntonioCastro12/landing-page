fdee3b72ff9644fce616168f347690fc62499c45
AntonioCastro1163 <antoniojcc786@gmail.com>
2024-07-18 18:15:50 -0600

    Nueva actualización

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (form) {
         form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Detener el envío del formulario por defecto
+            event.preventDefault(); 
 
-            // Mostrar la barra de carga
+            
             loadingOverlay.style.display = 'flex';
 
-            // Crear un objeto FormData con los datos del formulario
+            
             const formData = new FormData(form);
 
-            // Enviar el formulario usando fetch
+            
             fetch(form.action || '', {
                 method: 'POST',
                 body: formData,
@@ -22,12 +22,12 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .then(response => {
                 if (response.ok) {
-                    // Si el envío fue exitoso, reemplazar el contenido del contenedor
+                  
                     const formContainer = document.querySelector('.registro');
                     if (formContainer) {
                         formContainer.innerHTML = `
                             <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; background-color: #FCBB04;">
-                                       <iframe src="https://www.youtube.com/embed/HwXOBXsPnEE?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="position: absolute; top: 50%; left: 50%; width: 70%; height: 70%; transform: translate(-50%, -50%); background-color: #FCBB04; border: none;"></iframe>
+                                <iframe src="https://www.youtube.com/embed/HwXOBXsPnEE?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="position: absolute; top: 50%; left: 50%; width: 70%; height: 70%; transform: translate(-50%, -50%); background-color: #FCBB04; border: none;"></iframe>
                             </div>`;
                         
                         formContainer.style.backgroundColor = '#FCBB04';
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .catch(error => console.error('Error en la solicitud:', error))
             .finally(() => {
-                // Ocultar la barra de carga después de recibir la respuesta
+                
                 loadingOverlay.style.display = 'none';
             });
         });
